140a68aee699d0557e076b6fcc65cfec19d54e51
Art Tofan <arto@queue-it.com>
2024-07-08 16:54:13 +0200

    added some deps on libs

--- apps/app-3/src/app/services/dummy.service.ts
+++ apps/app-3/src/app/services/dummy.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { DummyService as SharedDummyService } from '@lib/library-c';
 
 @Injectable({ providedIn: 'root' })
 export class DummyService {
+  public sharedDummyService = inject(SharedDummyService);
+
   public getMessage(): string {
+    const sharedMessage = this.sharedDummyService.getMessage();
+
+    console.log('Shared message:', sharedMessage);
     return 'Hello from App DummyService 3!';
   }
 }
