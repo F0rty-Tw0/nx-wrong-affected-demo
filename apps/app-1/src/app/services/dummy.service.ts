3b2e8fea4d80d63e60d7aafc20cd6bd40964d401
root <root@LAPTOP-MUQ6E75B>
2024-07-10 22:10:05 +0200

    downgraded nx

--- apps/app-1/src/app/services/dummy.service.ts
+++ apps/app-1/src/app/services/dummy.service.ts
@@ -1,14 +1,8 @@
 import { inject, Injectable } from '@angular/core';
-import { DummyService as SharedDummyService } from '@lib/library-a';
 
 @Injectable({ providedIn: 'root' })
 export class DummyService {
-  public sharedDummyService = inject(SharedDummyService);
-
   public getMessage(): string {
-    const sharedMessage = this.sharedDummyService.getMessage();
-
-    console.log('Shared message:', sharedMessage);
     return 'Hello from App DummyService 1!';
   }
 }
